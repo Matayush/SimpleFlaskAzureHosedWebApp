ac03ff644c28ba2a8f006e1fdce2d4cad9f47074
Mateusz Ceniuk <ceniuk.mateusz@gmail.com>
2026-06-03 14:57:27 +0000

    Add HTML line breaks

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Mateusz,\n\nBest regards,\nWorld"
+    return "Hello Mateusz,<br><br>Best regards,<br>World"
 
 if __name__ == "__main__":
     app.run(debug=True)
