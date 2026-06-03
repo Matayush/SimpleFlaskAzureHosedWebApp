e41a6dabbb29c0b838cc74560b520744dd465c9c
Mateusz Ceniuk <ceniuk.mateusz@gmail.com>
2026-06-03 14:54:39 +0000

    message change

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Azure App Service!\n"
+    return "Hello Mateusz,\n\nBest regards,\nWorld"
 
 if __name__ == "__main__":
     app.run(debug=True)
